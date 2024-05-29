b24ea92af327807ccdc0883b8d6453dc14cb5848
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-29 11:55:25 +0100

    Define Post routes in the server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const usersRoutes = require('./routes/users');
+const postsRoutes = require('./routes/posts');
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Define Routes
 app.use('/api/users', usersRoutes);
+app.use('/api/posts', postsRoutes);
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
