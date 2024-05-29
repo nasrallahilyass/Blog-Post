9f54b3e262a1ff82b19cdaf4a84b82749b8ca1b9
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-29 12:48:48 +0100

    Define Comment routes in the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const usersRoutes = require('./routes/users');
 const postsRoutes = require('./routes/posts');
+const commentsRoutes = require('./routes/comments')
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 // Define Routes
 app.use('/api/users', usersRoutes);
 app.use('/api/posts', postsRoutes);
+app.use('/api/comments', commentsRoutes)
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
