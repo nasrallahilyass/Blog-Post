f0e851a6b0849d951193babe4cb0b126fbdabfe9
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-29 14:18:14 +0100

    validate user using express-validator

---
+++ server/validators/validateUser.js
@@ -0,0 +1,7 @@
+const { check } = require('express-validator');
+
+exports.validateUser = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Password must be 6 or more characters').isLength({ min: 6 }),
+];
