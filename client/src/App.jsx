60f122b2c46db57c30dec2673edea1aa90167b63
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-30 12:42:11 +0100

    App.jsx and main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,15 @@
 import './App.css'
+
+// import Login from './components/Pages/Login.'
+import Register from './components/Pages/Register'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login/> */}
+      <Register/>
+    </div>
+  )
+}
+
+export default App
