579058066918550e5c86eb80e1f449ad69ce7f5a
nasrallahilyass <nasrallah.dev@gmail.com>
2024-05-30 13:02:29 +0100

    check the login and register routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 
-// import Login from './components/Pages/Login.'
-import Register from './components/Pages/Register'
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Login/> */}
-      <Register/>
+      <h1 className='font-bold text-center mt-20 text-4xl'>Welcome To my App</h1>
+      {/* add two links to the login and register pages */}
+      <div className='flex justify-center mt-10'>
+        <a href="/login" className='bg-slate-900 text-white font-bold py-2 px-4 rounded'>Login</a>
+        <a href="/register" className='bg-slate-900  text-white font-bold py-2 px-4 rounded ml-4'>Register</a>
+      <div/>
     </div>
+  </div>
   )
 }
 
