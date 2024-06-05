993dbaebeb497d0ee79765f4226c24a70116db1a
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-05 12:25:28 +0100

    Fix the width

--- client/components/Services.jsx
+++ client/components/Services.jsx
@@ -30,7 +30,7 @@ const servicesData = [
 
 const Services = () => {
   return (
-    <section className='mb-12 xl:mb-36 mt-8'>
+    <section className='mb-12 xl:mb-36'>
       <div className='container mx-auto'>
         <h2 className='section-title mb-12 xl:mb-24 text-center mx-auto'>
           My Services
