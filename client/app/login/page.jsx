93b8b7fb5862a29feb9e283c32754a5eec2c27ed
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-06 12:43:22 +0100

    Create login page. v2.0

---
+++ client/app/login/page.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+// components
+import LoginForm from "@/components/LoginForm";
+const register = () => {
+  return (
+    <section>
+      <div className="container mx-auto lg:flex lg:justify-center xl:flex xl:justify-center my-20">
+        {/* text */}
+        <div className="flex flex-col justify-center lg:w-1/2 xl:w-1/2">
+          <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+            <span className="w-[30px] h-[2px] bg-primary"></span>
+            Welcome Back 👋
+          </div>
+          <h1 className="h1 max-w-md mb-8">Login Now</h1>
+          <p className="subtitle max-w-[400px]">
+            Begin shaping your tech blogs, showcasing your knowledge and
+            experiences, igniting curiosity, fostering learning, and connecting
+            with readers through valuable content and engaging narratives.
+          </p>
+        </div>
+        {/* form */}
+        <div className="w-full lg:w-1/2 lg:flex justify-center items-center">
+          <LoginForm />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default register;
---
+++ client/components/LoginForm.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { User, MailIcon, ArrowRightIcon, LockKeyhole } from "lucide-react";
+
+const LoginForm = () => {
+  return (
+    <form className="flex flex-col gap-y-4">
+      {/* input */}
+      <div className="relative flex items-center">
+        <Input type="name" id="name" placeholder="Name" />
+        <User className="absolute right-6" size={20} />
+      </div>
+      {/* input: email */}
+      <div className="relative flex items-center">
+        <Input type="email" id="email" placeholder="Email" />
+        <MailIcon className="absolute right-6" size={20} />
+      </div>
+
+      {/* input: password */}
+      <div className="relative flex items-center">
+        <Input type="password" id="password" placeholder="Password" />
+        <LockKeyhole className="absolute right-6" size={20} />
+      </div>
+        <Button className='my-3' type="submit">
+             Login Now
+          <ArrowRightIcon size={20} color="white" className="ml-3" />
+        </Button>
+        <div className="flex gap-x-2 justify-center mb-3">
+            <p>Don't have an account?</p>
+            <a href="/register" className="underline uppercase">Register Here!</a>
+        </div>
+    </form>
+  );
+};
+
+export default LoginForm;
