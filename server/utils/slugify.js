599c4839810bdeb67ac09df0061048a845bff45b
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-12 23:53:08 +0100

    Slugify fun

--- server/utils/slugify.js
+++ server/utils/slugify.js
@@ -1,9 +1,11 @@
-// utils/slugify.js
 const slugify = (text) => {
+  if (!text) {
+    throw new Error('Input text is undefined');
+  }
   return text
     .toLowerCase()
-    .replace(/[^\w ]+/g, '')
-    .replace(/ +/g, '-');
+    .replace(/[^\w\s-]+/g, '') // Allow spaces and hyphens
+    .replace(/ +/g, '-'); // Replace spaces with hyphens
 };
 
 module.exports = slugify;
