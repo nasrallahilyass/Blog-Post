3818ae6bd0cd704b442885da2d58886e168da6cd
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-12 11:28:14 +0100

    Connecting the client and the server

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*' // Proxy to Backend
-      }
+        source: '/api/auth/:path*', // Ensure NextAuth.js routes are not proxied
+        destination: '/api/auth/:path*',
+      },
+      {
+        source: '/api/:path*', // Proxy other API routes to the backend server
+        destination: 'http://localhost:5000/api/:path*',
+      },
     ];
-  }
+  },
 };
 
 module.exports = nextConfig;
