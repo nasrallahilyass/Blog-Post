08574fef6a24af1d7bc87663e05d4553888c5aa7
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-12 23:47:38 +0100

    categories controller with prisma

--- server/controllers/categoriesController.js
+++ server/controllers/categoriesController.js
@@ -1,14 +1,15 @@
-const Category = require('../models/Category');
+const prisma = require('../utils/connect');
 
 // Create a new category
 exports.createCategory = async (req, res) => {
   const { title, image } = req.body;
   try {
-    const category = new Category({ title, image });
-    await category.save();
+    const category = await prisma.category.create({
+      data: { title, image },
+    });
     res.status(201).json(category);
   } catch (error) {
-    console.error("Error during category creation:", error); // Log the error
+    console.error("Error during category creation:", error);
     res.status(500).send('Server Error');
   }
 };
@@ -16,7 +17,7 @@ exports.createCategory = async (req, res) => {
 // Get all categories
 exports.getCategories = async (req, res) => {
   try {
-    const categories = await Category.find();
+    const categories = await prisma.category.findMany();
     res.json(categories);
   } catch (err) {
     console.log(err.message);
@@ -27,7 +28,9 @@ exports.getCategories = async (req, res) => {
 // Get category by ID
 exports.getCategoryById = async (req, res) => {
   try {
-    const category = await Category.findById(req.params.id);
+    const category = await prisma.category.findUnique({
+      where: { id: req.params.id },
+    });
     if (!category) {
       return res.status(404).json({ msg: "Category not found" });
     }
@@ -42,11 +45,10 @@ exports.getCategoryById = async (req, res) => {
 exports.updateCategory = async (req, res) => {
   const { title, image } = req.body;
   try {
-    const category = await Category.findByIdAndUpdate(
-      req.params.id,
-      { title, image },
-      { new: true }
-    );
+    const category = await prisma.category.update({
+      where: { id: req.params.id },
+      data: { title, image },
+    });
     res.json({ msg: "Category Updated", category });
   } catch (err) {
     console.log(err.message);
@@ -57,7 +59,9 @@ exports.updateCategory = async (req, res) => {
 // Delete category
 exports.deleteCategory = async (req, res) => {
   try {
-    await Category.findByIdAndDelete(req.params.id);
+    await prisma.category.delete({
+      where: { id: req.params.id },
+    });
     res.json({ msg: "Category Deleted" });
   } catch (err) {
     console.log(err.message);
