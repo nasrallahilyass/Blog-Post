f637d8c5323fa3df343cc560ad0f07abc8096e13
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-12 23:52:46 +0100

    update the server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const prisma = require('./utils/connect')
 // Load environment variables from .env file
 dotenv.config();
 
@@ -16,15 +15,9 @@ app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
-})); 
+}));
 
-
-// Connect to MongoDB Atlas
-const db = process.env.DATABASE_URL;
-mongoose.connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log('MongoDB Connection Error:', err));
-
+// Use routes
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/categories', categoryRoutes);
@@ -33,4 +26,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Ensure to disconnect Prisma client when exiting the process
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
