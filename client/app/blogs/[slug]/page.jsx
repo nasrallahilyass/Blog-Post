f66f83373c080168634eed123f6c374ff6424229
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-15 12:11:42 +0100

    Single Post adding time and author

--- client/app/blogs/[slug]/page.jsx
+++ client/app/blogs/[slug]/page.jsx
@@ -1,5 +1,7 @@
 "use client"
 import { usePathname } from 'next/navigation';
+import format from "date-fns/format";
+
 
 import useSWR from "swr";
 import Spinner from '@/components/Spinner';
@@ -20,7 +22,11 @@ const SinglePost = () => {
   if (error) return <div>Error loading post: {error.message}</div>;
   if (!post) return <Spinner />;
 
+  const formattedDate = format(new Date(post.createdAt), "MMMM dd, yyyy");
+
+
   return (
+    
     <div className="max-w-4xl mx-auto p-4 sm:p-6 lg:p-8">
       <h1 className="text-3xl text-center sm:text-4xl font-bold mb-7 text-gray-900 dark:text-white">
         {post.title}
@@ -30,6 +36,10 @@ const SinglePost = () => {
         src={post.image}
         alt={post.title}
       />
+      <div className="flex justify-between items-center mb-7">
+        <span className="text-gray-800 dark:text-white font-semibold">By: {post.user.name}</span>
+        <span className="text-gray-600 dark:text-gray-300">{formattedDate}</span>
+      </div>
       <div
         className="text-lg sm:text-xl text-gray-700 leading-relaxed dark:text-white"
         dangerouslySetInnerHTML={{ __html: post.description }}
