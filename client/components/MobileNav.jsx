220f4a557f8745ec93b29ab00cb47be0bb6cf7f1
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-15 12:02:55 +0100

    adding user icon to the navBar

--- client/components/MobileNav.jsx
+++ client/components/MobileNav.jsx
@@ -1,45 +1,32 @@
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { AlignJustify } from 'lucide-react';
-import { signOut, useSession } from "next-auth/react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { AlignJustify } from "lucide-react";
+import { useSession } from "next-auth/react";
 
-import Nav from './Nav';
-import Logo from './Logo';
-import Socials from './Socials';
+import Nav from "./Nav";
+import Logo from "./Logo";
+import Socials from "./Socials";
+import { User } from "./User";
 
 const MobileNav = () => {
-  const {data, status} = useSession();
-
-  const handleGoogleLogout = (e) => {
-    e.preventDefault();
-    signOut('google');
-  };
-
+  const { data: session, status } = useSession();
   return (
     <Sheet>
       <SheetTrigger asChild>
-      <div className='flex gap-x-6 cursor-pointer'>
-        {
-          status === 'authenticated' && (
-          <div className="flex gap-x-2">
-            <a onClick={handleGoogleLogout} className='font-bold'>Log out</a> 
-            <a href="/new-post" className='font-medium'>Create Post</a>
-          </div>
-          
-          )
-        }
-        <AlignJustify className='cursor-pointer' />
-      </div>
+        <div className="flex gap-x-6 cursor-pointer">
+          {status === "authenticated" && <User />}
+          <AlignJustify className="mt-2 cursor-pointer" />
+        </div>
       </SheetTrigger>
       <SheetContent>
-        <div className='flex flex-col items-center justify-between h-full py-8'>
-          <div className='flex flex-col items-center gap-y-12'>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-12">
             <Logo />
             <Nav
-              containerStyles='flex flex-col items-center gap-y-6'
-              linkStyles='text-xl'
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-xl"
             />
           </div>
-          <Socials containerStyles='flex gap-x-4' iconsStyles='text-2xl' />
+          <Socials containerStyles="flex gap-x-4" iconsStyles="text-2xl" />
         </div>
       </SheetContent>
     </Sheet>
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -7,38 +7,50 @@ import { usePathname } from 'next/navigation';
 // framer motion
 import { motion } from 'framer-motion';
 
+// next-auth
+import { useSession } from 'next-auth/react';
+
+// user component
+import { User } from './User';
+
 const links = [
   { path: '/', name: 'home' },
   { path: '/projects', name: 'my projects' },
   { path: '/blogs', name: 'my Blogs' },
   { path: '/contact', name: 'contact' },
-  { path: '/auth', name: 'auth'}
 ];
 
 const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
   const path = usePathname();
+  const { data: session, status } = useSession();
+
   return (
     <nav className={`${containerStyles}`}>
-      {links.map((link, index) => {
-        return (
-          <Link
-            href={link.path}
-            key={index}
-            className={`capitalize ${linkStyles}`}
-          >
-            {link.path === path && (
-              <motion.span
-                initial={{ y: '-100%' }}
-                animate={{ y: 0 }}
-                transition={{ type: 'tween' }}
-                layoutId='underline'
-                className={`${underlineStyles}`}
-              />
-            )}
-            {link.name}
-          </Link>
-        );
-      })}
+      {links.map((link, index) => (
+        <Link
+          href={link.path}
+          key={index}
+          className={`capitalize ${linkStyles}`}
+        >
+          {link.path === path && (
+            <motion.span
+              initial={{ y: '-100%' }}
+              animate={{ y: 0 }}
+              transition={{ type: 'tween' }}
+              layoutId='underline'
+              className={`${underlineStyles}`}
+            />
+          )}
+          {link.name}
+        </Link>
+      ))}
+      {status === 'authenticated' ? (
+        <User />
+      ) : (
+        <Link href="/auth" className={`capitalize ${linkStyles}`}>
+          auth
+        </Link>
+      )}
     </nav>
   );
 };
