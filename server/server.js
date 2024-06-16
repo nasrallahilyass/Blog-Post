0df33a6156d96fab2f97d79f96b74c59e3feabbc
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-16 14:32:58 +0100

    add cors for blog

--- server/server.js
+++ server/server.js
@@ -1,28 +1,30 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const prisma = require('./utils/connect');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const prisma = require("./utils/connect");
 
 // Load environment variables from .env file
 dotenv.config();
 
-const postRoutes = require('./routes/posts');
-const commentRoutes = require('./routes/comments');
-const categoryRoutes = require('./routes/categories');
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+const categoryRoutes = require("./routes/categories");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://blog-post-pi-seven.vercel.app'],
-    methods: 'GET,POST,PUT,DELETE',
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://blog-post-pi-seven.vercel.app",  'https://blog-post-pnq4bl5xo-nasrallahs-projects.vercel.app'],
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 
 // Use routes
-app.use('/api/posts', postRoutes);
-app.use('/api/comments', commentRoutes);
-app.use('/api/categories', categoryRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,7 +33,7 @@ app.listen(PORT, () => {
 });
 
 // Ensure to disconnect Prisma client when exiting the process
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await prisma.$disconnect();
   process.exit(0);
 });
