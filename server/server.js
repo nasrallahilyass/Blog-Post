03c0e508fb903d9e5ee0792e4908b28a06180da1
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-16 14:24:51 +0100

    CORS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://blog-post-pi-seven.vercel.app'],
     methods: 'GET,POST,PUT,DELETE',
 }));
 
