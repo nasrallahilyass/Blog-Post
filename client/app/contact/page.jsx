5e690ab15dee7d286b346fcb96bb59783b0f9bc6
nasrallahilyass <nasrallah.dev@gmail.com>
2024-06-16 12:48:44 +0100

    Update contact page

--- client/app/contact/page.jsx
+++ client/app/contact/page.jsx
@@ -21,8 +21,7 @@ const Contact = () => {
             </div>
             <h1 className='h1 max-w-md mb-8'>Let's Work Together.</h1>
             <p className='subtitle max-w-[400px]'>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat,
-              nostrum.
+              Feel free to get in touch with me. I am always open to discussing new projects, creative ideas, or opportunities to be part of your visions.  
             </p>
           </div>
           {/* illustration */}
@@ -30,24 +29,6 @@ const Contact = () => {
         </div>
         {/* info text & form */}
         <div className='grid xl:grid-cols-2 mb-24 xl:mb-32'>
-          {/* info text */}
-          <div className='flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg'>
-            {/* mail */}
-            <div className='flex items-center gap-x-8'>
-              <MailIcon size={18} className='text-primary' />
-              <div>youremail@gmail.com</div>
-            </div>
-            {/* address */}
-            <div className='flex items-center gap-x-8'>
-              <HomeIcon size={18} className='text-primary' />
-              <div>8420 Pine Avenue, Meadowville MC 842</div>
-            </div>
-            {/* phone */}
-            <div className='flex items-center gap-x-8'>
-              <PhoneCall size={18} className='text-primary' />
-              <div>+44 123 546 7890</div>
-            </div>
-          </div>
           <ContactForm />
         </div>
       </div>
